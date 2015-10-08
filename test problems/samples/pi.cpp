1222ca155dd0c7a7afa6fde74351e80d0b45bee2
xflying777 <xflying777@yahoo.com.tw>
2015-10-08 16:12:28 +0800

    update pi

--- test problems/samples/pi.cpp
+++ test problems/samples/pi.cpp
@@ -6,15 +6,17 @@
 int main()
 {
 	int i, n;
-	double pi, t;
-	n = 10000;
+	double pi, t, t1, t2;
+	n = 100000000;
 	pi = 0.0;
 	
+	t1 = clock();
 	#pragma acc kernels
 	for(i=0;i<n;i++)
 	{
 		t = (i + 0.5)/n;
 		pi = pi + 4.0/(1.0 + t*t);
 	}
-	printf("pi = %f \n", pi/n);
+	t2 = clock();
+	printf("pi = %f \n times = %f \n", pi/n, 1.0*(t2 - t1)/CLOCKS_PER_SEC);
 }
