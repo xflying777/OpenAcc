1c3db04570b312946a20a5b32f08b0e1623c4c8d
xflying777 <xflying777@yahoo.com.tw>
2015-10-13 15:38:09 +0800

    times

--- test problems/matrix vector/matrix_multiplication2.cpp
+++ test problems/matrix vector/matrix_multiplication2.cpp
@@ -1,11 +1,13 @@
 /* matrix-acc-func.c */
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #define SIZE 1000
  
 void doTest(float *restrict a, float *restrict b, float *restrict c, int size)
 {
 	int i,j,k;
+	float t1, t2, gpu_times;
 	
 	#pragma acc kernels create(a[0:size*size], b[0:size*size]) copyout(c[0:size*size]) 
 	{
@@ -22,6 +24,7 @@ void doTest(float *restrict a, float *restrict b, float *restrict c, int size)
 		}	
 	}
 	
+	t1 = clock();
 	// Compute matrix multiplication.
 	#pragma acc loop independent
 	for (i = 0; i < size; ++i) 
@@ -36,16 +39,19 @@ void doTest(float *restrict a, float *restrict b, float *restrict c, int size)
 			}
 		}
 	}
-}
+	t2 = clock();
+	printf("gpu times = %f \n", 1.0*(t2-t1)/CLOCKS_PER_SEC);
+	}
 }
 	
 int main()
 {
-	int i,j,k;
-	int size=SIZE;
-	float *a= (float*)malloc(sizeof(float)*size*size);
-	float *b= (float*)malloc(sizeof(float)*size*size);
-	float *c= (float*)malloc(sizeof(float)*size*size);
+	int i, j, k;
+	int size = SIZE;
+	float t1, t2, cpu_times;
+	float *a = (float*)malloc(sizeof(float)*size*size);
+	float *b = (float*)malloc(sizeof(float)*size*size);
+	float *c = (float*)malloc(sizeof(float)*size*size);
 	
 	
 	doTest(a,b,c, size);
@@ -62,11 +68,15 @@ int main()
 		for(j = 0; j < size; ++j) 
 			seq[i*SIZE+j] = 0.f;
 	
+	t1 = clock();
 	// Perform the multiplication
 	for (i = 0; i < size; ++i) 
 		for (j = 0; j < size; ++j) 
 			for (k = 0; k < size; ++k) 
 				seq[i*size+j] += (i+k) * (k-j);
+	t2 = clock();
+	printf("cpu times = %f \n", 1.0*(t2-t1)/CLOCKS_PER_SEC);
+	
 	
 	// check all the OpenACC matrices
 	for (i = 0; i < size; ++i)
