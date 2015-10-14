af3eaeee2ce52472631b7be79cba6b214cc3a9e6
xflying777 <xflying777@yahoo.com.tw>
2015-10-14 16:07:19 +0800

    bit reverse test

--- test problems/fast fourier transform/bitreverse.cpp
+++ test problems/fast fourier transform/bitreverse.cpp
@@ -3,16 +3,16 @@
 #include <math.h>
 #include <time.h>
 
-void cpuFFTr2(double *x_r, double *x_i, double *y_r, double *y_i, int N);
-void gpuFFTr2(double *x_r, double *x_i, double *y_r, double *y_i, int N);
-void error(double *y_r, double *y_i, double *z_r, double *z_i, int N);
+void cpu_bit_reverse(double *x_r, double *x_i, double *y_r, double *y_i, int N);
+void gpu_bit_reverse(double *x_r, double *x_i, double *y_r, double *y_i, int N);
 void Initial(double *x, double *y, int N);
-void Print_Complex_Vector(double *y_r, double *y_i, int N);
 int Generate_N(int p);
+void error(double *y_r, double *y_i, double *z_r, double *z_i, int N);
+void Print_Complex_Vector(double *y_r, double *y_i, int N);
 
 int main()
 {
-	int p, N;
+	int i, n, p, N;
 	double *y_r, *y_i, *z_r, *z_i, *x_r, *x_i, cpu_times, gpu_times;
 	clock_t t1, t2;
 	
@@ -31,21 +31,20 @@ int main()
 	Initial(x_r, x_i, N);
 	
 	t1 = clock();
-	cpuFFTr2(x_r, x_i, y_r, y_i, N);
+	cpu_bit_reverse(x_r, x_i, y_r, y_i, N);
 	t2 = clock();
 	cpu_times = 1.0*(t2-t1)/CLOCKS_PER_SEC;
 	
 	t1 = clock();
-	gpuFFTr2(x_r, x_i, z_r, z_i, N);
+	gpu_bit_reverse(x_r, x_i, z_r, z_i, N);
 	t2 = clock();
-	gpu_times = 1.0*(t2-t1)/CLOCKS_PER_SEC;	
+	gpu_times = 1.0*(t2-t1)/CLOCKS_PER_SEC;
 	
-	printf("cpu times = %f \n gpu_times = %f \n cpu times / gpu times = %f \n", cpu_times, gpu_times, cpu_times/gpu_times);
+	printf("cpu times = %f \n gpu times = %f \n cpu times / gpu times = %f \n", cpu_times, gpu_times, cpu_times/gpu_times);
 	error(y_r, y_i, z_r, z_i, N);
-//	Print_Complex_Vector(y_r, y_i, N);
 	
-	return 0;
-} 
+//	Print_Complex_Vector(y_r, y_i, N);
+}
 
 void Initial(double *x, double *y, int N)
 {
@@ -57,6 +56,13 @@ void Initial(double *x, double *y, int N)
 	}
 }
 
+int Generate_N(int p)
+{
+	int N = 1;
+	for(;p>0;p--) N*=2;
+	return N;
+}
+
 void Print_Complex_Vector(double *y_r, double *y_i, int N)
 {
 	int n;
@@ -65,13 +71,6 @@ void Print_Complex_Vector(double *y_r, double *y_i, int N)
 		if (y_i[n] >=0) printf("%d : %f +%f i\n", n, y_r[n], y_i[n]);
 		else printf("%d : %f %f i\n", n, y_r[n], y_i[n]);
 	}
-}
-
-int Generate_N(int p)
-{
-	int N = 1;
-	for(;p>0;p--) N*=2;
-	return N;
 }
 
 void error(double *y_r, double *y_i, double *z_r, double *z_i, int N)
@@ -89,12 +88,10 @@ void error(double *y_r, double *y_i, double *z_r, double *z_i, int N)
 		
 }
 
-void cpuFFTr2(double *x_r, double *x_i, double *y_r, double *y_i, int N)
+void cpu_bit_reverse(double *x_r, double *x_i, double *y_r, double *y_i, int N)
 {
-	// input : x = x_r + i * x_i
-	// output: y = y_r + i * y_i
 	int k, n, i, j, M;
-	double t_r, t_i;
+	double t_r;
 	
 	for(n=0;n<N;++n)
 	{
@@ -103,7 +100,7 @@ void cpuFFTr2(double *x_r, double *x_i, double *y_r, double *y_i, int N)
 	}
 	
 	i = j = 0;
-	while(i < N)
+	for(i=0;i<N;i++)
 	{
 		if(i < j)
 		{
@@ -119,56 +116,25 @@ void cpuFFTr2(double *x_r, double *x_i, double *y_r, double *y_i, int N)
 			M = M / 2;
 		}
 		j = j + M;		
-		i = i + 1;
 	}
-	// Butterfly structure
-	double theta, w_r, w_i;
-	n = 2;
-	while(n <= N)
-	{
-		for(k=0;k<n/2;k++)
-		{
-			theta = -2.0*k*M_PI/n;
-			w_r = cos(theta);
-			w_i = sin(theta);
-			for(i=k;i<N;i+=n)
-			{
-				j = i + n/2;
-				t_r = w_r * y_r[j] - w_i * y_i[j];
-				t_i = w_r * y_i[j] + w_i * y_r[j];
-				
-
-				y_r[j] = y_r[i] - t_r;
-				y_i[j] = y_i[i] - t_i;
-				y_r[i] = y_r[i] + t_r;
-				y_i[i] = y_i[i] + t_i;
-
-			}
-		}
-		n = n * 2;
-	}
-	
 }
 
-
-void gpuFFTr2(double *x_r, double *x_i, double *y_r, double *y_i, int N)
+void gpu_bit_reverse(double *x_r, double *x_i, double *y_r, double *y_i, int N)
 {
-	// input : x = x_r + i * x_i
-	// output: y = y_r + i * y_i
 	int k, n, i, j, M;
-	double t_r, t_i;
+	double t_r;
 	
-	#pragma acc data copyin(x_r[0:N], x_i[0:N]) copy(y_r[0:N], y_i[0:N])
+	#pragma acc data copyin(x_r[0:N], x_i[0:N])
 	#pragma acc parallel loop independent
 	for(n=0;n<N;++n)
 	{
 		y_r[n] = x_r[n];
 		y_i[n] = x_i[n];
 	}
-
+	
 	#pragma acc kernels
 	i = j = 0;
-	while(i < N)
+	for(i=0;i<N;i++)
 	{
 		if(i < j)
 		{
@@ -184,35 +150,28 @@ void gpuFFTr2(double *x_r, double *x_i, double *y_r, double *y_i, int N)
 			M = M / 2;
 		}
 		j = j + M;		
-		i = i + 1;
-	}
-	
-	#pragma acc kernels
-	// Butterfly structure
-	double theta, w_r, w_i;
-	n = 2;
-	while(n <= N)
-	{
-		for(k=0;k<n/2;k++)
-		{
-			theta = -2.0*k*M_PI/n;
-			w_r = cos(theta);
-			w_i = sin(theta);
-			for(i=k;i<N;i+=n)
-			{
-				j = i + n/2;
-				t_r = w_r * y_r[j] - w_i * y_i[j];
-				t_i = w_r * y_i[j] + w_i * y_r[j];
-				
-
-				y_r[j] = y_r[i] - t_r;
-				y_i[j] = y_i[i] - t_i;
-				y_r[i] = y_r[i] + t_r;
-				y_i[i] = y_i[i] + t_i;
-
-			}
-		}
-		n = n * 2;
 	}
 }
 
+/*void cpu_bit_reverse2(double *x_r, double *x_i, double *y_r, double *y_i, int N)
+{
+	int k, n, i, j, M, *temp;
+	double t;
+	
+	temp = (int *) malloc(N*sizeof(int));
+	temp[0] = 0;
+	
+	for(n=0;n<N;++n)
+	{
+		y_r[n] = x_r[n];
+		y_i[n] = x_i[n];
+	}
+	
+	M = N / 2;
+	for(i=0;i<N;i=i*2)
+	{
+		M = M / 2;
+		
+	}
+}
+*/
