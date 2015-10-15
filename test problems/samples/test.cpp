1cdf2f266d9fe7d66a39dac2ffd6a15c7726a269
xflying777 <xflying777@gmail.com>
2015-10-15 10:59:19 +0800

    test

---
+++ test problems/samples/test.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+void cputest1(int N, double *p);
+void gputest1(int N, double *p);
+
+int main()
+{
+	int N;
+	double cpu_t1, gpu_t1, *p, *q;
+	clock_t t1, t2;
+	
+	printf("Input N = ");
+	scanf("%d",&N);
+	
+	
+	t1 = clock();
+	cputest1(N, p);
+	t2 = clock();
+	cpu_t1 = 1.0*(t2-t1)/CLOCKS_PER_SEC;
+
+        t1 = clock();
+        gputest1(N, q);
+        t2 = clock();
+	gpu_t1 = 1.0*(t2-t1)/CLOCKS_PER_SEC;
+
+	printf("test1 cpu times = %f \n", cpu_t1);
+	printf("test1 gpu times = %f \n", gpu_t1);
+	printf(" p = %f \n q = %f \n", *p, *q);
+	return 0;
+}
+
+void cputest1(int N, double *p)
+{
+	int i;
+	*p = 1.0;
+	for(i=0;i<N;i++)
+	{
+		*p = *p + 1;
+	}
+}
+
+void gputest1(int N, double *p)
+{
+        int i;
+        *p = 1.0;
+	#pragma acc kernels
+        for(i=0;i<N;i++)
+        {
+                *p = *p + 1;
+        }
+}
