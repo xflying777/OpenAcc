3fcd014560693a15a554d10207eb70e9dc4fdda9
xflying777 <xflying777@yahoo.com.tw>
2015-11-25 14:05:04 +0800

    cufft test

---
+++ test_problems/fast_fourier_transform/cufft_test.c
@@ -0,0 +1,60 @@
+#include <cufft.h>
+
+// Declared extern "C" to disable C++ name mangling
+extern "C" void launchCUFFT(float *d_data, int n, void *stream)
+{
+    cufftHandle plan;
+    cufftPlan1d(&plan, n, CUFFT_C2C, 1);
+    cufftSetStream(plan, (cudaStream_t)stream);
+    cufftExecC2C(plan, (cufftComplex*)d_data, (cufftComplex*)d_data,CUFFT_FORWARD);
+    cufftDestroy(plan);
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "openacc.h"
+
+#ifndef M_PI
+#define M_PI           3.14159265358979323846
+#endif
+
+// Forward declaration of wrapper function that will call CUFFT
+extern void launchCUFFT(float *d_data, int n, void *stream);
+
+int main(int argc, char *argv[])
+{
+    int n = 256;
+    float *data = malloc(2*n*sizeof(float));
+    int i;
+
+    // Initialize interleaved input data on host
+    float w = 7.0;
+    float x;
+    for(i=0; i<2*n; i+=2)  {
+        x = (float)i/2.0/(n-1);
+        data[i] = cos(2*M_PI*w*x);
+        data[i+1] = 0.0;
+    }
+
+    // Copy data to device at start of region and back to host and end of region
+    #pragma acc data copy(data[0:2*n])
+    {
+        // Inside this region the device data pointer will be used
+        #pragma acc host_data use_device(data)
+        {
+           void *stream = acc_get_cuda_stream(acc_async_sync);
+           launchCUFFT(data, n, stream);
+        }
+    }
+
+    // Find the frequency
+    int max_id = 0;
+    for(i=0; i<n; i+=2) {
+        if( data[i] > data[max_id] )
+            max_id = i;
+    }
+    printf("frequency = %d\n", max_id/2);
+
+    return 0;
+}
