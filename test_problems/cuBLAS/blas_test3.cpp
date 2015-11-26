c487479859b0ed6f82d496d68447cb66535a5a94
xflying777 <xflying777@yahoo.com.tw>
2015-11-26 16:21:26 +0800

    make up

---
+++ test_problems/cuBLAS/blas_test3.cpp
@@ -0,0 +1,109 @@
+#include "cublas_v2.h"
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int gpu_cublas3( const int n, const double *a, const double *b, double *c )
+{
+	cublasStatus_t stat = CUBLAS_STATUS_SUCCESS;
+	#pragma acc data pcopyin( a[0:n*n], b[0:n*n] ) pcopyout( c[0:n*n] )
+	{
+		#pragma acc host_data use_device( a, b, c )
+		{
+			cublasHandle_t handle;
+			stat = cublasCreate(&handle);
+			if ( CUBLAS_STATUS_SUCCESS != stat ) {
+				printf("CUBLAS initialization failed\n");
+			}
+			
+			if ( CUBLAS_STATUS_SUCCESS == stat )
+			{
+				const double alpha = 1.0;
+				const double beta = 0.0;
+				stat = cublasDgemm( handle, CUBLAS_OP_N, CUBLAS_OP_N, n,n,n, &alpha, a, n, b, n, &beta, c, n);
+				if (stat != CUBLAS_STATUS_SUCCESS) {
+					printf("cublasDgemm failed\n");
+				}
+			}
+			cublasDestroy(handle);
+		}
+	}
+	return CUBLAS_STATUS_SUCCESS == stat;
+}
+void gpu_oacc(int n, double *a, double *b, double *c)
+{
+	int i,j,k;
+	// Compute mAtrix multipliCAtion.
+	#pragma acc data copyin(a[0:n*n],b[0:n*n]) copy(c[0:n*n])
+	#pragma acc kernels
+	#pragma acc loop independent
+	for (i = 0; i < n; ++i) 
+	{
+	#pragma acc loop independent
+		for (j = 0; j < n; ++j) 
+		{
+	#pragma acc loop seq
+			for (k = 0; k < n; ++k) 
+			{
+				c[i*n+j] += a[i*n+k] * b[k*n+j];
+			}
+		}
+	}
+}
+
+int main()
+{
+	int i, j, n;
+	double gpu_cublas3_times, gpu_oacc_times;
+	clock_t t1, t2;
+
+	printf("Input size n = ");
+	scanf("%d",&n);
+
+	double *a = (double*)malloc(sizeof(double)*n*n);
+	double *b = (double*)malloc(sizeof(double)*n*n);
+	double *c_gpu_cublas3 = (double*)malloc(sizeof(double)*n*n);
+	double *c_gpu_oacc = (double*)malloc(sizeof(double)*n*n);
+
+	for (i = 0; i < n; ++i)
+	{
+		for (j = 0; j < n; ++j)
+		{
+			a[i*n+j] = i + j;
+			b[i*n+j] = i - j;
+		}
+	}
+	t1 = clock();
+	gpu_oacc( n, a, b, c_gpu_oacc);
+	t2 = clock();
+	gpu_oacc_times = 1.0*(t2-t1)/CLOCKS_PER_SEC;
+
+	t1 = clock();
+	#pragma acc data copyin( a[0:n*n], b[0:n*n] ) copyout( c_gpu_cublas3[0:n*n] )
+	{
+		gpu_cublas3( n, a, b, c_gpu_cublas3);
+	}
+	t2 = clock();
+	gpu_cublas3_times = 1.0*(t2-t1)/CLOCKS_PER_SEC;
+
+	int nfailures = 0;
+	printf(" %lf %lf\n", c_gpu_cublas3[0], c_gpu_cublas3[n*n-1]);
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j) 
+		{
+	    	if (c_gpu_oacc[i*n+j] != c_gpu_cublas3[i*n+j]) nfailures++;
+		}
+	}
+	if (nfailures != 0)
+      	printf(" Test FAILED\n");
+	else
+	{
+      	printf(" Test tblas6 PASSED\n");
+		printf(" gpu oacc times = %f \n", gpu_oacc_times);
+		printf(" gpu cublas3 times = %f \n", gpu_cublas3_times);
+		printf(" gpu oacc times/gpu cublas3 times = %f \n", gpu_oacc_times/gpu_cublas3_times);
+	}
+	return nfailures;
+}
---
+++ test_problems/cuFFT/cufft_test6.cpp
@@ -0,0 +1,49 @@
+#include <cufft.h>
+
+// Forward declaration of wrapper function that will call CUFFT
+// Declared extern "C" to disable C++ name mangling
+extern "C" void for_CUFFT(float *d_data, int n, void *stream)
+{
+    cufftHandle plan;
+    cufftPlan1d(&plan, n, CUFFT_C2C, 1);
+    cufftSetStream(plan, (cudaStream_t)stream);
+    cufftExecC2C(plan, (cufftComplex*)d_data, (cufftComplex*)d_data,CUFFT_FORWARD);
+    cufftDestroy(plan);
+}
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "openacc.h"
+
+int main(int argc, char *argv[])
+{
+    int n = 256;
+    float *data = (float* )malloc(2*n*sizeof(float));
+    int i;
+    
+    // Initialize interleaved input data on host
+    for(i=0; i<2*n; i+=2)  
+	{
+        data[i] = i/2.0;
+        data[i+1] = 0.0;
+    }
+
+    // Copy data to device at start of region and back to host and end of region
+    #pragma acc data copy(data[0:2*n])
+    {
+        // Inside this region the device data pointer will be used
+        #pragma acc host_data use_device(data)
+        {
+           void *stream = acc_get_cuda_stream(acc_async_sync);
+           for_CUFFT(data, n, stream);
+        }
+    }
+
+    for(i=0; i<n; i+=2) {
+		printf(" data[%d] = %f \n", i, data[i]);
+    }
+
+    return 0;
+}
