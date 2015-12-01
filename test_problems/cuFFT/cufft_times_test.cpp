421bdc73d373666ed0e3d975fabea763f256bb0d
xflying777 <xflying777@gmail.com>
2015-12-01 16:38:14 +0800

    cuFFT successed

--- test_problems/cuFFT/cufft_times_test.cpp
+++ test_problems/cuFFT/cufft_times_test.cpp
@@ -5,17 +5,18 @@
 #include <cufft.h>
 #include "openacc.h"
 
-extern "C" void forward_cuFFT(double *d_data, int N, void *stream);
-int FFTr2(double *x_r, double *x_i, double *y_r, double *y_i, int N);
-int Initial(double *x, double *y, int N);
-int Print_Complex_Vector(double *y_r, double *y_i, double *data, int N);
+extern "C" void forward_cuFFT(float *d_data, int N, void *stream);
+void FFTr2(double *x_r, double *x_i, double *y_r, double *y_i, int N);
+void Initial(double *x, double *y, int N);
+void Print_Complex_Vector(double *y_r, double *y_i, float *data, int N);
 int Generate_N(int p);
 
 int main()
 {
-	int k, n, p, N;
-	double *y_r, *y_i, *x_r, *x_i, w_r, w_i, *data;
-	double cpu_fft_times, gpu_cufft_times;
+	int i, p, N;
+	double *y_r, *y_i, *x_r, *x_i;
+	float *data;
+	double cpu_FFT_times, gpu_cuFFT_times;
 	clock_t t1, t2;
 	
 	printf("Please input p =");
@@ -27,31 +28,31 @@ int main()
 	x_i = (double *) malloc(N*sizeof(double));
 	y_r = (double *) malloc(N*sizeof(double));
 	y_i = (double *) malloc(N*sizeof(double));
-	data = (double *) malloc(2*N*sizeof(double));
+	data = (float *) malloc(2*N*sizeof(float));
 	
 	
 	// Initialize interleaved input data on host
-    for(i=0; i<2*N; i+=2)  
+	for(i=0; i<2*N; i+=2)  
 	{
-        data[i] = i/2.0;
-        data[i+1] = 0.0;
-    }
+        	data[i] = i/2.0;
+        	data[i+1] = 0.0;
+    	}
 	Initial(x_r, x_i, N);
 	
 	// Test cpu_FFT and gpu_cuFFT
 	// Copy data to device at start of region and back to host and end of region
 	t1 = clock();
-    #pragma acc data copy(data[0:2*N])
-    {
+    	#pragma acc data copy(data[0:2*N])
+    	{
         // Inside this region the device data pointer will be used
         #pragma acc host_data use_device(data)
-        {
-           void *stream = acc_get_cuda_stream(acc_async_sync);
-           forward_cuFFT(data, N, stream);
-        }
-    }
-    t2 = clock();
-    gpu_cuFFT_times = 1.0*(t2-t1)/CLOCKS_PER_SEC;
+        	{
+           		void *stream = acc_get_cuda_stream(acc_async_sync);
+           		forward_cuFFT(data, N, stream);
+        	}
+    	}
+    	t2 = clock();
+    	gpu_cuFFT_times = 1.0*(t2-t1)/CLOCKS_PER_SEC;
     
 	t1 = clock();
 	FFTr2(x_r, x_i, y_r, y_i, N);
@@ -64,12 +65,12 @@ int main()
 	printf(" gpu cuFFT: %f secs \n", gpu_cuFFT_times);
 	printf(" cpu FFT / gpu cuFFT: %f times \n", cpu_FFT_times / gpu_cuFFT_times);
 	printf(" \n");
-	Print_Complex_Vector(y_r, y_i, data, N);
+//	Print_Complex_Vector(y_r, y_i, data, N);
 	
 	return 0;
 } 
 
-int Initial(double *x, double *y, int N)
+void Initial(double *x, double *y, int N)
 {
 	int i;
 	for(i=0;i<N;++i)
@@ -79,20 +80,19 @@ int Initial(double *x, double *y, int N)
 	}
 }
 
-int Print_Complex_Vector(double *y_r, double *y_i, double *data, int N)
+void Print_Complex_Vector(double *y_r, double *y_i, float *data, int N)
 {
 	int i;
 	for(i=0; i<N; ++i)
 	{
-		if (y_i[i] >=0) printf("%d : %f + %f i\n", n, y_r[i], y_i[i]);
-		else printf("fft[%d]: %f  %f i \n", n, y_r[i], y_i[i]);
+		if (y_i[i] >=0) printf(" cpu_fft%d : %f + %f i\n", i, y_r[i], y_i[i]);
+		else printf(" cpu_fft[%d]: %f  %f i \n", i, y_r[i], y_i[i]);
 	}
+	printf(" \n");
 	for(i=0; i<2*N; i+=2) 
 	{
 		printf(" cufft_data[%d] = %f + %f i \n", i/2, data[i], data[i+1]);
-    }
-	
-	return 0;
+    	}
 }
 
 int Generate_N(int p)
@@ -102,7 +102,7 @@ int Generate_N(int p)
 	return N;
 }
 
-int FFTr2(double *x_r, double *x_i, double *y_r, double *y_i, int N)
+void FFTr2(double *x_r, double *x_i, double *y_r, double *y_i, int N)
 {
 	// input : x = x_r + i * x_i
 	// output: y = y_r + i * y_i
@@ -162,12 +162,11 @@ int FFTr2(double *x_r, double *x_i, double *y_r, double *y_i, int N)
 		}
 		n = n * 2;
 	}
-	
 }
 
 // Forward declaration of wrapper function that will call CUFFT
 // Declared extern "C" to disable C++ name mangling
-extern "C" void forward_cuFFT(double *d_data, int N, void *stream)
+extern "C" void forward_cuFFT(float *d_data, int N, void *stream)
 {
     cufftHandle plan;
     cufftPlan1d(&plan, N, CUFFT_C2C, 1);
