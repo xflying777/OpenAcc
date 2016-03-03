97657acb3e7fef04a0381fb3e951e66f65add615
xflying777 <xflying777@yahoo.com.tw>
2016-03-03 14:10:05 +0800

    norm test
    test seq and reduction

--- samples/norm.cpp
+++ samples/norm.cpp
@@ -15,7 +15,7 @@ double norm_cpu(double *x)
 	return norm_x; 
 }
 
-double norm_gpu(double *x)
+double norm_gpu_seq(double *x)
 {
 	int n, i;
 	double norm_x;
@@ -28,9 +28,28 @@ double norm_gpu(double *x)
 	return norm_x; 
 }
 
+double norm_gpu_reduction(double *x)
+{
+	int n, i;
+	double norm_x;
+
+	n = sizeof(x)/sizeof(x[0]);
+	#pragma acc data present(x) create(norm_x)
+	{
+		norm_x = 0.0;
+		#pragma acc update device(norm_x)
+		
+		#pragma acc parallel loop reduction(+:norm_x)
+		for (i=0; i<n; i++)	norm_x += x[i]*x[i];
+	}
+	#pragma acc update host(norm_x)
+	return norm_x; 
+}
+
 void initial(double *x, int N)
 {
 	int i;
+	#pragma acc data copyout(x[0:N])
 	for (i=0; i<N; i++)	x[i] = sin(i);
 }
 
@@ -40,7 +59,7 @@ int main()
 	printf(" Input N = ");
 	scanf("%d", &N);
 
-	double *x, cpu, gpu, error, t1, t2, cpu_time, gpu_time;
+	double *x, cpu, gpu_seq, gpu_reduction, error, t1, t2, cpu_time, gpu_seq_time, gpu_reduction_time;
 	x = (double *) malloc(N*sizeof(double));
 
 	initial(x, N);
@@ -52,15 +71,23 @@ int main()
 	t1 = clock();
 	#pragma acc data copyin(x[0:N])
 	{
-	gpu = norm_gpu(x);
+		gpu_seq = norm_gpu_seq(x);
 	}
 	t2 = clock();
-	gpu_time = 1.0*(t2 - t1)/CLOCKS_PER_SEC;
+	gpu_seq_time = 1.0*(t2 - t1)/CLOCKS_PER_SEC;
+	
+	t1 = clock();
+	#pragma acc data copyin(x[0:N])
+	{
+		gpu_reduction = norm_gpu_reduction(x);
+	}
+	t2 = clock();
+	gpu_reduction_time = 1.0*(t2 - t1)/CLOCKS_PER_SEC;
 
-	error = fabs(cpu - gpu);
+	error = fabs((cpu - gpu_seq) + (cpu - gpu_reduction));
 	printf(" error = %f \n", error);
-	printf(" cpu times = %f , gpu times = %f \n", cpu_time, gpu_time);
-	printf(" cpu/gpu = %f \n", cpu_time/gpu_time);
+	printf(" cpu times = %f , gpu seq times = %f , gpu reduction times = %f \n", cpu_time, gpu_seq_time, gpu_reduction_times);
+	//printf(" cpu/gpu = %f \n", cpu_time/gpu_time);
 
 	return 0;
 }
