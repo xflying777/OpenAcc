8b72a3f302226b134d5187200b80870c8e99d6f3
xflying777 <xflying777@yahoo.com.tw>
2016-03-03 15:17:48 +0800

    norm cublas

--- samples/norm.cpp
+++ samples/norm.cpp
@@ -14,18 +14,6 @@ double norm_cpu(double *x, int N)
 	return norm_x;
 }
 
-double norm_gpu_seq(double *x, int N)
-{
-	int i;
-	double norm_x;
-
-	norm_x = 0.0;
-	#pragma acc parallel loop seq present(x)
-	for (i=0; i<N; i++)	norm_x += x[i]*x[i];
-
-	return norm_x;
-}
-
 double norm_gpu_reduction(double *x, int N)
 {
 	int i;
@@ -41,11 +29,33 @@ double norm_gpu_reduction(double *x, int N)
 	return norm_x;
 }
 
+double norm_gpu_cublas(const double *x, int N)
+{
+	double nrm2;
+	#pragma acc data copyin(x[0:N])
+	{
+		#pragma acc host_data use_device(x)
+		{
+			cublasHandle_t handle;
+			int incx;
+			incx = 1;
+			cublasDnrm2(handle, N, x, incx, nrm2);
+			cublasDestroy(handle);
+		}
+	}
+	
+	return *nrm2;
+}
+
+//******************************************************************************************************
+
 void initial(double *x, int N)
 {
 	int i;
 	for (i=0; i<N; i++)	x[i] = sin(i);
 }
+
+//******************************************************************************************************
 
 int main()
 {
@@ -53,7 +63,7 @@ int main()
 	printf(" Input N = ");
 	scanf("%d", &N);
 
-	double *x, cpu, gpu_seq, gpu_reduction, error, t1, t2, cpu_time, gpu_seq_time, gpu_reduction_time;
+	double *x, cpu, gpu_cublas, gpu_reduction, error, t1, t2, cpu_time, gpu_cublas_time, gpu_reduction_time;
 	x = (double *) malloc(N*sizeof(double));
 
 	initial(x, N);
@@ -67,10 +77,10 @@ int main()
 	t1 = clock();
 	#pragma acc data copyin(x[0:N])
 	{
-		gpu_seq = norm_gpu_seq(x, N);
+		gpu_cublas = norm_gpu_cublas(x, N);
 	}
 	t2 = clock();
-	gpu_seq_time = 1.0*(t2 - t1)/CLOCKS_PER_SEC;
+	gpu_cublas_time = 1.0*(t2 - t1)/CLOCKS_PER_SEC;
 	
 	t1 = clock();
 	#pragma acc data copyin(x[0:N])
@@ -80,10 +90,10 @@ int main()
 	t2 = clock();
 	gpu_reduction_time = 1.0*(t2 - t1)/CLOCKS_PER_SEC;
 
-	error = fabs((cpu - gpu_seq) + (cpu - gpu_reduction));
+	error = fabs((cpu - gpu_cublas) + (cpu - gpu_reduction));
 	printf("\n norm = %f \n", cpu);
 	printf(" error = %f \n", error);
-	printf(" cpu times = %f , gpu seq times = %f , gpu reduction times = %f \n", cpu_time, gpu_seq_time, gpu_reduction_time);
+	printf(" cpu times = %f , gpu cublas times = %f , gpu reduction times = %f \n", cpu_time, gpu_cublas_time, gpu_reduction_time);
 	//printf(" cpu/gpu = %f \n", cpu_time/gpu_time);
 
 	return 0;
