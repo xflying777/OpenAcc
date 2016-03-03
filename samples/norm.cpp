d75a5f04384ca2858a1235e7f37ba0a1e62faffd
xflying777 <xflying777@gmail.com>
2016-03-03 14:39:01 +0800

    test norm

--- samples/norm.cpp
+++ samples/norm.cpp
@@ -3,53 +3,47 @@
 #include <math.h>
 #include <time.h>
 
-double norm_cpu(double *x)
+double norm_cpu(double *x, int N)
 {
-	int n, i;
+	int i;
 	double norm_x;
 
-	n = sizeof(x)/sizeof(x[0]);
 	norm_x = 0.0;
-	for (i=0; i<n; i++)	norm_x += x[i]*x[i];
+	for (i=0; i<N; i++)	norm_x = norm_x + x[i]*x[i];
 
-	return norm_x; 
+	return norm_x;
 }
 
-double norm_gpu_seq(double *x)
+double norm_gpu_seq(double *x, int N)
 {
-	int n, i;
+	int i;
 	double norm_x;
 
-	n = sizeof(x)/sizeof(x[0]);
 	norm_x = 0.0;
 	#pragma acc parallel loop seq present(x)
-	for (i=0; i<n; i++)	norm_x += x[i]*x[i];
+	for (i=0; i<N; i++)	norm_x += x[i]*x[i];
 
-	return norm_x; 
+	return norm_x;
 }
 
-double norm_gpu_reduction(double *x)
+double norm_gpu_reduction(double *x, int N)
 {
-	int n, i;
+	int i;
 	double norm_x;
 
-	n = sizeof(x)/sizeof(x[0]);
-	#pragma acc data present(x) create(norm_x)
+	#pragma acc data present(x) copyout(norm_x)
 	{
 		norm_x = 0.0;
 		#pragma acc update device(norm_x)
-		
 		#pragma acc parallel loop reduction(+:norm_x)
-		for (i=0; i<n; i++)	norm_x += x[i]*x[i];
+		for (i=0; i<N; i++)	norm_x += x[i]*x[i];
 	}
-	#pragma acc update host(norm_x)
-	return norm_x; 
+	return norm_x;
 }
 
 void initial(double *x, int N)
 {
 	int i;
-	#pragma acc data copyout(x[0:N])
 	for (i=0; i<N; i++)	x[i] = sin(i);
 }
 
@@ -63,15 +57,17 @@ int main()
 	x = (double *) malloc(N*sizeof(double));
 
 	initial(x, N);
+	printf("\n x[1] = %f \n", x[1]);
+
 	t1 = clock();
-	cpu = norm_cpu(x);
+	cpu = norm_cpu(x, N);
 	t2 = clock();
 	cpu_time = 1.0*(t2 - t1)/CLOCKS_PER_SEC;
 
 	t1 = clock();
 	#pragma acc data copyin(x[0:N])
 	{
-		gpu_seq = norm_gpu_seq(x);
+		gpu_seq = norm_gpu_seq(x, N);
 	}
 	t2 = clock();
 	gpu_seq_time = 1.0*(t2 - t1)/CLOCKS_PER_SEC;
@@ -79,14 +75,15 @@ int main()
 	t1 = clock();
 	#pragma acc data copyin(x[0:N])
 	{
-		gpu_reduction = norm_gpu_reduction(x);
+		gpu_reduction = norm_gpu_reduction(x, N);
 	}
 	t2 = clock();
 	gpu_reduction_time = 1.0*(t2 - t1)/CLOCKS_PER_SEC;
 
 	error = fabs((cpu - gpu_seq) + (cpu - gpu_reduction));
+	printf("\n norm = %f \n", cpu);
 	printf(" error = %f \n", error);
-	printf(" cpu times = %f , gpu seq times = %f , gpu reduction times = %f \n", cpu_time, gpu_seq_time, gpu_reduction_times);
+	printf(" cpu times = %f , gpu seq times = %f , gpu reduction times = %f \n", cpu_time, gpu_seq_time, gpu_reduction_time);
 	//printf(" cpu/gpu = %f \n", cpu_time/gpu_time);
 
 	return 0;
