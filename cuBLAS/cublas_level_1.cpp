bca9b89171fd1cc5c2120c5cda11edcf1d554bc2
xflying777 <xflying777@gmail.com>
2016-03-09 12:00:31 +0800

    cublas level 1 success !

--- cuBLAS/cublas_level_1.cpp
+++ cuBLAS/cublas_level_1.cpp
@@ -17,7 +17,7 @@
 
 //************************************************************************
 void initial(double *A, double *B, int N);
-void error(double *A, double *B, int N);
+double error(double *A, double *B, int N);
 //************************************************************************
 void cublas_gemm(const double *A, const double *B, double *C, int N);
 //************************************************************************
@@ -36,7 +36,7 @@ int main()
 	int N, N2;
 	printf(" \n Input matrix size N x N, N = ");
 	scanf("%d", &N);
-	printf(" N = %d \n", N);
+	printf(" N = %d \n \n", N);
 	N2 = N*N;
 
 	double *A, *B, *C_cpu, *C_gpu, *D_cpu, *D_gpu, t1, t2, cpu_time, gpu_time;
@@ -47,40 +47,42 @@ int main()
 	C_cpu = (double *) malloc(N2*sizeof(double));
 	C_gpu = (double *) malloc(N2*sizeof(double));
 	D_cpu = (double *) malloc(N2*sizeof(double));
-	D_cpu = (double *) malloc(N2*sizeof(double));
-	
+	D_gpu = (double *) malloc(N2*sizeof(double));
+
 	r_gpu = (double *) malloc(1*sizeof(double));
 	nrmC_gpu = (double *) malloc(1*sizeof(double));
 
 	initial(A, B, N);
 
 	t1 = clock();
-	
+
 	#pragma acc data copyin(A[0:N2], B[0:N2]) copyout(C_cpu[0:N2])
 	{
 		cublas_gemm(A, B, C_cpu, N);
 	}
 	r_cpu = dot_cpu(C_cpu, B, N2);
 	axpy_cpu(-1.0*r_cpu, B, C_cpu, N2);
-	nrmC_cpu = norm(C_cpu, N2);
-	copy_cpu(C_cpu, D, N2);
-	scal_cpu(1.0/nrmC_cpu, D, N2);
-	
+	nrmC_cpu = norm_cpu(C_cpu, N2);
+	copy_cpu(C_cpu, D_cpu, N2);
+	scal_cpu(1.0/nrmC_cpu, D_cpu, N2);
+
 	t2 = clock();
 	cpu_time = 1.0*(t2 - t1)/CLOCKS_PER_SEC;
-	
+
 	t1 = clock();
-	
-	#pragma acc enter data copyin(A[0:N2], B[0:N2]) create(C_gpu[0:N2], D_gpu[0:N2], r_gpu[0], nrmC_gpu[0]) 
+
+	#pragma acc enter data copyin(A[0:N2], B[0:N2]) create(C_gpu[0:N2], r_gpu[0], nrmC_gpu[0], D_gpu[0:N2])
 	{
 		gpu_cublas1(A, B, C_gpu, D_gpu, r_gpu, nrmC_gpu, N, N2);
 	}
 	#pragma acc update host(D_gpu[0:N2])
-	
+
 	t2 = clock();
 	gpu_time = 1.0*(t2 - t1)/CLOCKS_PER_SEC;
+	printf(" gpu part success \n");
 
-	printf(" error = %f \n", error(D_cpu, D_gpu, N2));
+
+	printf(" \n error = %f \n", error(D_cpu, D_gpu, N2));
 	printf(" gpu time = %f, cpu times = %f \n", gpu_time, cpu_time);
 
 	return 0;
@@ -102,20 +104,21 @@ void initial(double *A, double *B, int N)
 	}
 }
 
-void error(double *A, double *B, int N)
+double error(double *A, double *B, int N)
 {
 	int i;
-	double error, temp;
-	
-	error = 0.0;
+	double e, temp;
+
+	e = 0.0;
 	for (i=0; i<N; i++)
 	{
 		temp = fabs(A[i] - B[i]);
-		if (temp > error)	error = temp;
+		if (temp > e)	e = temp;
 	}
-	
-	return error;
+
+	return e;
 }
+
 //************************************************************************
 
 void cublas_gemm(const double *A, const double *B, double *C, int N)
@@ -138,7 +141,7 @@ void cublas_gemm(const double *A, const double *B, double *C, int N)
 
 void gpu_cublas1(double *A, double *B, double *C, double *D, double *r, double *nrmC, int N, int N2)
 {
-	#pragma acc data present(A, B, C, D, r, nrmC)
+	#pragma acc data present(A, B, C, D)
 	{
 		#pragma acc host_data use_device(A, B, C, D)
 		{
@@ -147,14 +150,21 @@ void gpu_cublas1(double *A, double *B, double *C, double *D, double *r, double *
 			const double alpha = 1.0;
 			const double beta = 0.0;
 			cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_T, N, N, N, &alpha, A, N, B, N, &beta, C, N);
+			printf(" gpu gemm success \n");
 			cublasDdot(handle, N2, C, 1, B, 1, r);
+			printf(" gpu dot success \n");
 			*r = -1.0 * *r;
 			cublasDaxpy(handle, N2, r, B, 1, C, 1);
+			printf(" gpu axpy success \n");
 			cublasDnrm2(handle, N2, C, 1, nrmC);
-			cublasDcopy(hnadle, N2, C, 1, D, 1);
+			printf(" gpu nrm2 success \n");
+			cublasDcopy(handle, N2, C, 1, D, 1);
+			printf(" gpu copy success \n");
 			*nrmC = 1.0 / *nrmC;
 			cublasDscal(handle, N2, nrmC, D, 1);
+			printf(" gpu scal success \n");
 			cublasDestroy(handle);
+			printf(" gpu destroy success \n");
 		}
 	}
 }
@@ -168,7 +178,7 @@ double norm_cpu(double *A, int N)
 	double norm;
 
 	norm = 0.0;
-	for (i=0; i<N*N; i++)	norm += A[i]*A[i];
+	for (i=0; i<N; i++)	norm += A[i]*A[i];
 
 	norm = sqrt(norm);
 
