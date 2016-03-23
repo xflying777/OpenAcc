e3ec22c88de7f8b101972d169429b8da6b440846
xflying777 <xflying777@yahoo.com.tw>
2016-03-23 17:30:16 +0800

    Arnoldi test

--- Arnoldi_Iteration/Arnoldi_gpu3.cpp
+++ Arnoldi_Iteration/Arnoldi_gpu3.cpp
@@ -1,11 +1,11 @@
-//***********************************************************************
-// Parallel modefied Gram-Schmidt part.
-//***********************************************************************
+//*******************************************************
+// Only parallel matrix mutplication.
+//*******************************************************
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
-#include "cblas.h"
+#include "cublas_v2.h"
 
 void initial(double *A, double *b, int N);
 void Arnoldi_Iteration(double *A, double *Q, double *H, double *b, int N, int iter);
@@ -55,75 +55,75 @@ void norm(double *x, double *nrm, int N)
 {
 	int i;
 	double temp;
-
-	#pragma acc data present(x)
-	{
-		temp = 0.0;
-		#pragma acc parallel loop reduction(+:temp)
-		for (i=0; i<N; i++)	temp += x[i]*x[i];
-		*nrm = sqrt(temp);
-	}
-}
-
-void norm_cpu(double *x, double *nrm, int N)
-{
-	int i;
-	double temp;
 	temp = 0.0;
 	for (i=0; i<N; i++)	temp += x[i]*x[i];
 	*nrm = sqrt(temp);
 }
- 
+
+// Ax = b
+void matrix_matrix(double *A, double *x, double *b, int N)
+{
+	#pragma acc data copyin(A[0:N*N], x[0:N*N]) copyout(b[0:N*N])
+	{
+		#pragma acc host_data use_device(A, x, b)
+		{
+			cublasHandle_t h;
+			cublasCreate(&h);
+			const double alpha = 1.0;
+			const double beta = 0.0;
+			cublasDgemm(h, CUBLAS_OP_T, CUBLAS_OP_T, N, N, N, &alpha, A, N, x, N, &beta, b, N);
+			cublasDestroy(h);
+		}
+	}
+//	printf(" cublasDgemm success \n");
+}
+
 //***********************************************************************************
 
 void Arnoldi_Iteration(double *A, double *Q, double *H, double *b, int N, int iter)
 {
 	int i, j, k;
 	double *v, *q;
-	double *nrm, *dot, temp;
+	double *nrm, temp;
 
 	v = (double *) malloc(N*N*sizeof(double));
 	q = (double *) malloc(N*N*sizeof(double));
 
 	nrm = (double *) malloc(1*sizeof(double));
-	dot = (double *) malloc(1*sizeof(double));
 
-	norm_cpu(b, nrm, N*N);
-	for (k=0; k<N*N; k++)	Q[k] = b[k] / *nrm;
+	norm(b, nrm, N*N);
+	temp = *nrm;
+	for (k=0; k<N*N; k++)	Q[k] = b[k]/temp;
 
+	t1 = clock();
 	for (i=0; i<iter; i++)
 	{
 		// v= A*qi
 		for (k=0; k<N*N; k++)	q[k] = Q[N*N*i+k];
-		cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, N, N, N, 1.0, A, N, q, N, 0.0, v, N);
+		matrix_matrix(A, q, v, N);
 
 		// h(j,i) = qj*v
-		#pragma acc data copy(Q[0:N*N*(iter+1)], H[0:(iter+1)*iter]) copyin(v[0:N*N])
-		{ 
-			#pragma acc parallel loop independent
-			for (j=0; j<=i; j++)
-			{
-				temp = 0.0;
-				#pragma acc loop reduction(+:temp)
-				for (k=0; k<N*N; k++)	temp += Q[N*N*j+k]*v[k];
-				H[iter*j+i] = temp;
-			}
+		for (j=0; j<=i; j++)
+		{
+			temp = 0.0;
+			for (k=0; k<N*N; k++)	temp += Q[N*N*j+k]*v[k];
+			H[iter*j+i] = temp;
+		}
 
-			// v = v - \sum h(j,i)*qj
-			#pragma acc parallel loop seq
-			for (j=0; j<=i; j++)
-			{
-				#pragma acc loop independent
-				for (k=0; k<N*N; k++)	v[k] -= H[iter*j+i]*Q[N*N*j+k];
-			}
+		// v = v - \sum h(j,i)*qj
+		for (j=0; j<=i; j++)
+		{
+			for (k=0; k<N*N; k++)	v[k] -= H[iter*j+i]*Q[N*N*j+k];
+		}
 
-			// h(i+1,i) = ||v||
-			norm(v, nrm, N*N);
-			H[iter*(i+1)+i] = *nrm;
-			// qi+1 = v/h(i+1,i)
-			#pragma acc parallel loop independent
-			for (k=0; k<N*N; k++)	Q[N*N*(i+1)+k] = v[k] / *nrm;
-		} //end pragma acc
+		// h(i+1,i) = ||v||
+		norm(v, nrm, N*N);
+		H[iter*(i+1)+i] = *nrm;
+		temp = *nrm;
+		// qi+1 = v/h(i+1,i)
+		for (k=0; k<N*N; k++)	Q[N*N*(i+1)+k] = v[k] / temp;
 	}
+	t2 = clock();
+	printf(" Arnoldi times = %f \n", 1.0*(t2-t1)/CLOCKS_PER_SEC);
 }
 
