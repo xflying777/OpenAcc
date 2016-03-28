25815230a9301764d859aa79405a51d70cadfa6e
xflying777 <xflying777@yahoo.com.tw>
2016-03-28 15:50:24 +0800

    test

--- Arnoldi_Iteration/test.cpp
+++ Arnoldi_Iteration/test.cpp
@@ -13,7 +13,8 @@
 void initial(double *Q, double *v_cpu, double *v_gpu, int N, int iter);
 double error(double *x, double *y, int N);
 void cpu_test(double *Q, double *H, double *v, int N, int iter);
-void gpu_test(double *Q, double *H, double *v, int N, int iter);
+void oacc_test(double *Q, double *H, double *v, int N, int iter);
+void cublas_test(double *Q, double *H, double *v, int N, int iter);
 
 int main()
 {
@@ -25,7 +26,7 @@ int main()
 	printf(" N = %d, max_iter = %d \n\n", N, iter);
 
 	double *Q, *H_oacc, *H_cublas, *H_cpu, *v_oacc, *v_cublas, *v_cpu;
-	double t1, t2, cpu_time, gpu_time;
+	double t1, t2, cpu_time, oacc_time, cublas_time;
 
 	Q = (double *) malloc(N*N*iter*sizeof(double));
 	H_cpu = (double *) malloc(iter*sizeof(double));
@@ -35,8 +36,7 @@ int main()
 	v_oacc = (double *) malloc(N*N*sizeof(double));
 	v_cublas = (double *) malloc(N*N*sizeof(double));
 
-	initial(Q, v_cpu, v_gpu, N, iter);
-	printf(" v error = %e \n\n", error(v_gpu, v_cpu, N*N));
+	initial(Q, v_cpu, v_oacc, v_cublas, N, iter);
 
 	t1 = clock();
 	cpu_test(Q, H_cpu, v_cpu, N, iter);
@@ -44,26 +44,33 @@ int main()
 	cpu_time = 1.0*(t2-t1)/CLOCKS_PER_SEC;
 
 	t1 = clock();
-	gpu_test(Q, H_gpu, v_gpu, N, iter);
+	oacc_test(Q, H_oacc, v_oacc, N, iter);
 	t2 = clock();
-	gpu_time = 1.0*(t2-t1)/CLOCKS_PER_SEC;
+	oacc_time = 1.0*(t2-t1)/CLOCKS_PER_SEC;
 
-	printf(" H error = %e, v error = %e \n", error(H_cpu, H_gpu, iter), error(v_cpu, v_gpu, N*N));
-	printf(" cpu time = %f, gpu time = %f \n\n", cpu_time, gpu_time);
+	t1 = clock();
+	cublas_test(Q, H_cublas, v_cublas, N, iter);
+	t2 = clock();
+	cublas_time = 1.0*(t2-t1)/CLOCKS_PER_SEC;
+
+	printf(" (oacc) H error = %e, v error = %e \n", error(H_cpu, H_oacc, iter), error(v_cpu, v_oacc, N*N));
+	printf(" (cublas) H error = %e, v error = %e \n", error(H_cpu, H_cublas, iter), error(v_cpu, v_cublas, N*N));
+	printf(" cpu time = %f, oacc time = %f,  cublas time = %f \n\n", cpu_time, oacc_time, cublas_time);
 
 	return 0;
 }
 
 //**********************************************************************************************
 
-void initial(double *Q, double *v_cpu, double *v_gpu, int N, int iter)
+void initial(double *Q, double *v_cpu, double *v_oacc, double *v_cublas, int N, int iter)
 {
 	int i;
 	for (i=0; i<N*N*iter; i++)	Q[i] = sin(i);
 	for (i=0; i<N*N; i++)
 	{
 		v_cpu[i] = i;
-		v_gpu[i] = v_cpu[i];
+		v_oacc[i] = v_cpu[i];
+		v_cublas[i] = v_cpu[i];
 	}
 }
 
@@ -100,7 +107,7 @@ void cpu_test(double *Q, double *H, double *v, int N, int iter)
 	}
 }
 
-void gpu_test(double *Q, double *H, double *v, int N, int iter)
+void oacc_test(double *Q, double *H, double *v, int N, int iter)
 {
 	int i, j;
 
@@ -112,6 +119,40 @@ void gpu_test(double *Q, double *H, double *v, int N, int iter)
 			H[i] = 0.0;
 			#pragma acc loop seq
 			for (j=0; j<N*N; j++)	H[i] += Q[N*N*i+j]*v[j];
+		}
+
+		#pragma acc parallel loop seq
+		for (i=0; i<iter; i++)
+		{
+			#pragma acc loop independent
+			for (j=0; j<N*N; j++)	v[j] -= H[i]*Q[N*N*i+j];
+		}
+	} //end pragma acc
+}
+
+void cublas_test(double *Q, double *H, double *v, int N, int iter)
+{
+	int i, j;
+	double *temp, *dot;
+
+	temp = (double *) malloc(N*N*sizeof(double));
+	dot = (double *) malloc(1*sizeof(double));
+
+	#pragma acc data copyin(Q[0:N*N*iter]) copy(H[0:iter], v[0:N*N]) create(temp[0:N*N])
+	{
+		#pragma acc host_data use_device(temp, v)
+		{
+			cublasHandle_t h;
+			cublasCreate(&h);
+			for (i=0; i<iter; i++)
+			{
+				#pragma acc parallel loop independent
+				for (j=0; j<N*N; j++)	temp[j] = Q[N*N*i+j];
+
+				cublasDdot(h, N*N, temp, 1, v, 1, dot);
+				H[i] = *dot;
+			}
+			cublasDestroy(h);
 		}
 
 		#pragma acc parallel loop seq
